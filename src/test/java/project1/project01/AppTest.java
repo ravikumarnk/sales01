791f4fc053edbdb04aec29f289ce6ed6b73dafa3
ravikumar <ravikelakam@gmail.com>
2024-08-26 11:18:26 +0530

    first commit

---
+++ src/test/java/project1/project01/AppTest.java
@@ -0,0 +1,21 @@
+package project1.project01;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+     
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("calling test method.. ");
+    }
+    
+    
+    
+}
